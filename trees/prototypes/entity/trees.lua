e055b97fb8cf3101e7938bb8e36db92082cd739c
Sean Troxel <sean.troxel@parivedasolutions.com>
2023-10-30 17:58:40 -0500

    increase tree mining count to 80 and 40 increase wood stack size to 200 add info.json

---
+++ trees/prototypes/entity/trees.lua
@@ -0,0 +1,12 @@
+-- loops through all trees
+for t, _ in pairs(data.raw["tree"]) do
+    -- distinguish between live and dead trees
+    if data.raw["tree"][t].minable.count == 4 then
+        data.raw["tree"][t].minable.count = 80
+    else
+        data.raw["tree"][t].minable.count = 40
+    end
+end
+
+-- increase stack size to handle picking up all this wood
+data.raw.item["wood"].stack_size = 200
